a47d4994bc1d1fecb3500906747e3eaf53026b6e
Luke Garrigan <lukegarrigan8@gmail.com>
2019-05-08 18:05:40 +0100

    Added colour

--- public/Player.js
+++ public/Player.js
@@ -3,10 +3,12 @@ class Player {
     this.x = player.x;
     this.y = player.y;
     this.id = player.id;
+    this.rgb = player.rgb;
   }
 
 
   draw() {
+    fill(this.rgb.r, this.rgb.g, this.rgb.b);
     circle(this.x, this.y, 20);
   }
 
